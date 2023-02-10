5b785fa570cf12f30bf1b15c17c8ce7731a94007
Abhay <work.abhaypaswan@gmail.com>
2023-02-11 00:00:17 +0530

    added first machine coding problem

--- snake_and_ladder/main.py
+++ snake_and_ladder/main.py
@@ -24,12 +24,34 @@ class SnakeLadder:
 """
     Here is the sample inputs to create a board, if you want you can take these inputs from users.
 """
-ladders = {1: Ladder(1, 38), 4: Ladder(4, 14), 9: Ladder(9, 31), 21: Ladder(21, 42), 28: Ladder(28, 84), 36: Ladder(36, 44), 51: Ladder(51, 67), 71: Ladder(71, 91), 80: Ladder(80, 100)}
-snakes = {98: Snake(98, 78), 95: Snake(95, 75), 93: Snake(93, 73), 87: Snake(87, 24), 64: Snake(64, 60), 62: Snake(62, 19), 56: Snake(56, 53), 49: Snake(49, 11), 48: Snake(48, 26)}
 max_position = 100 
 start_position = 1
 no_of_players = 2
 no_of_dice = 1
+
+# Assumption: Ladders and Snakes won't make a loop.
+ladders = {
+    1: Ladder(1, 38), 
+    4: Ladder(4, 14), 
+    9: Ladder(9, 31), 
+    21: Ladder(21, 42), 
+    28: Ladder(28, 84), 
+    36: Ladder(36, 44), 
+    51: Ladder(51, 67), 
+    71: Ladder(71, 91), 
+    80: Ladder(80, 100)
+}
+snakes = {
+    98: Snake(98, 78), 
+    95: Snake(95, 75), 
+    93: Snake(93, 73), 
+    87: Snake(87, 24), 
+    64: Snake(64, 60), 
+    62: Snake(62, 19), 
+    56: Snake(56, 53), 
+    49: Snake(49, 11), 
+    48: Snake(48, 26)
+}
 
 
 # play the game
